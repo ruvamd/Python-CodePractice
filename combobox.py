ce3bf06b57c57a8cc141891cddf4d88dbf6f3217
Vadim <vadim@Vadims-MacBook-Pro.local>
2020-01-21 12:41:09 -0800

    1/21/20

---
+++ combobox.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk 
+root=Tk()
+month=StringVar()
+combobox=ttk.Combobox(root,textvariable=month)
+combobox.pack()
+combobox.config(values=('jan','feb','march','april','may',
+                        'jul','aug','sep','oct','nov','dec'))
+month.get()
+'mar'
+month.set('choose month')
+month.get()
+year=StringVar()
+Spinbox(root,from_=2000,to=2019,textvariable=year).pack()
+
+
---
+++ single line.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk 
+root=Tk()
+entry=ttk.Entry(root,width=30)
+entry.pack()
+entry.insert(0,'enter your password')
+entry.config(show='*')
+
