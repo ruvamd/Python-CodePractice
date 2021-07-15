fb749fcad49894f5c01dd75bcf87df782a35309d
vadim rusu <ruvamd@gmail.com>
2021-07-15 14:33:09 -0700

    hr array

---
+++ hackerRank/hr arrays.py
@@ -0,0 +1,19 @@
+import numpy
+
+num=numpy.array(input().split(),float)
+print(list(reversed(num)))
+
+'''
+import numpy
+
+def arrays(arr):
+    # complete this function
+    return numpy.array(arr[::-1],float)
+    
+    # use numpy.array
+   
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
+'''
