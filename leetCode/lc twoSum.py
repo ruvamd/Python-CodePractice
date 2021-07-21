3ba5002089127d08cf19d6001d751a7a858479a7
vadim rusu <ruvamd@gmail.com>
2021-07-20 17:39:54 -0700

    lc twoSum few more

--- leetCode/lc twoSum.py
+++ leetCode/lc twoSum.py
@@ -1,44 +1,59 @@
-# nums=[2,7,11,15]
-# target=9
 
-# for i in range(len(nums)):
-#     a=i+i
-#     if a==target:
-#         print(a.index())
-    
+#result with index,class
+# i is index,nums[i] is number from nums list
+
+# :type nums: List[int]
+# :type target: int
+# :rtype: List[int]
+
+nmrLst=[4,5,1,8]
+trg=9
+
+class solution:
+    def twoSum(self,nums,target):
+        hashTable={}
+        for i in range(len(nums)):
+            result=target-nums[i]
+            if result in hashTable:
+               return [hashTable[result],i]
+               #print([hashTable[result],i])
+            else: hashTable[nums[i]]=i
+
+print(solution().twoSum(nmrLst,trg))
+#solution().twoSum(nmrLst,trg)
 
 '''
-class Solution(object):
-   def twoSum(self, nums, target):
-      """
-      :type nums: List[int]
-      :type target: int
-      :rtype: List[int]
-      """
-      required = {}
-      for i in range(len(nums)):
-         if target - nums[i] in required:
-            return [required[target - nums[i]],i]
-         else:
-            required[nums[i]]=i
-input_list = [2,8,12,15]
-ob1 = Solution()
-print(ob1.twoSum(input_list, 20))
-'''
+#---result with numerals---#
 
 # Driver Code
-num_arr = [4, 5, 1, 8]
-pair_sum = 9  
+numArr = [4, 5, 1, 8]
+pairSum = 9  
 
-def twoSumHashing(num_arr, pair_sum):
-    sums = []
-    hashTable = {}
-
-    for i in range(len(num_arr)):
-        complement = pair_sum - num_arr[i]
-        if complement in hashTable:
-            print("Pair with sum", pair_sum,"is: (", num_arr[i],",",complement,")")
-        hashTable[num_arr[i]] = num_arr[i]  
+def twoSumHashing(numArr, pairSum):
+   sums = []
+   hashTable = {}
+   for i in range(len(numArr)):
+      result=pairSum-numArr[i]
+      if result in hashTable:
+         print(f'Because {result}+{numArr[i]}=={pairSum},we return{result,numArr[i]}')
+         #print([result,numArr[i]])
+      else: hashTable[numArr[i]]=numArr[i]  
   
 # Calling function
-twoSumHashing(num_arr, pair_sum)
+twoSumHashing(numArr, pairSum)
+'''
+
+#--two for loops--#
+#use return and call function to return one pair of indexes 
+nmrLst=[4,5,1,8]
+trg=9
+def bruteForceTwoSum(nums,target):
+   for i in range(len(nums)):
+      for j in range(i+1,len(nums)):
+         sum=nums[i]+nums[j]
+         if sum==target:
+            #return [i,j] #one index result
+            #print([i,j])  #two index result
+            print(nums[i],nums[j])
+#print(bruteForceTwoSum(nmrLst,trg)) #one index result
+bruteForceTwoSum(nmrLst,trg) #two index result
