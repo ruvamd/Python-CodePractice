386af70c2471805a3ddfd7f408f4c2fe9ffe8a31
vadim rusu <ruvamd@gmail.com>
2021-08-09 23:32:51 -0700

    hr Diff

--- hackerRank/hr .difference() operation.py
+++ hackerRank/hr .difference() operation.py
@@ -1,3 +1,6 @@
-numOfStdL,rollOfStdL=[set(input().split()) for _ in range(4)][1::2]
-print(len(numOfStdL-rollOfStdL))
-print(numOfStdL-rollOfStdL)
+numOfStdL,rollOfStdL=[set(input().split()) for _ in range(4)][1::2] #enter: 9
+                                                                          # 1 2 3 4 5 6 7 8 9
+                                                                          # 9
+                                                                          # 10 1 2 3 11 21 55 6 8
+print(len(numOfStdL-rollOfStdL)) #output: 4
+# print(numOfStdL-rollOfStdL)
