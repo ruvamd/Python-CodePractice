315765cd2b1ecf5fbb876fa516166d3c5ba40892
vadim rusu <ruvamd@gmail.com>
2021-08-23 16:50:27 -0700

    .

--- hackerRank/hr re.split().py
+++ hackerRank/hr re.split().py
@@ -1,14 +1,16 @@
 '''
-input: 100,000,000.000
-output: 
-100
-000
-000
-000
+input: 
+100,000,000.000
 '''
 import re
 
 rx=re.split(r'\D',input())
 for i in rx:
     print(i)
-
+'''
+output: 
+100
+000
+000
+000
+'''
