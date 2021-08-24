e35a1bc7cfc249d2ef2e052d126ddb6c5b32472d
vadim rusu <ruvamd@gmail.com>
2021-08-24 13:05:17 -0700

    .

--- hackerRank/hr Map and Lambda Function.py
+++ hackerRank/hr Map and Lambda Function.py
@@ -8,13 +8,13 @@ for i in range(5):#int(input())
     a,b=b,a+b    
 
 #---alt code---  
-cube = lambda x:pow(x,3) # complete the lambda function 
-def fibonacci(n):
-    # return a list of fibonacci numbers
-        lis = [0,1]
-        for i in range(2,n):
-            lis.append(lis[i-2] + lis[i-1])
-        return(lis[0:n])
+# cube = lambda x:pow(x,3) # complete the lambda function 
+# def fibonacci(n):
+#     # return a list of fibonacci numbers
+#         lis = [0,1]
+#         for i in range(2,n):
+#             lis.append(lis[i-2] + lis[i-1])
+#         return(lis[0:n])
 '''
 output: [0, 1, 1, 8, 27]
 '''
