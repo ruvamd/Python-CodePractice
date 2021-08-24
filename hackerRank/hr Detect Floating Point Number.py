26f50852160ca6f538197b52a7dd2495b801d34a
vadim rusu <ruvamd@gmail.com>
2021-08-24 13:30:53 -0700

    .

--- hackerRank/hr Detect Floating Point Number.py
+++ hackerRank/hr Detect Floating Point Number.py
@@ -1,3 +1,11 @@
+'''
+input:
+4
+4.0O0
+-1.00
++4.54
+SomeRandomStuff
+'''
 import re
 #+,-,. valid
 #at least one decimal ex:12.0
@@ -11,3 +19,10 @@ print(*x,sep='\n')
 
 # for _ in range(int(input())):
 # 	print(bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$',input())))
+'''
+output:
+False
+True
+True
+False
+'''
