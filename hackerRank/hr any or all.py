b89311101e602b8b4f2084083290866a504c66e8
vadim rusu <ruvamd@gmail.com>
2021-08-24 13:44:17 -0700

    .

--- hackerRank/hr any or all.py
+++ hackerRank/hr any or all.py
@@ -1,3 +1,8 @@
+'''
+input: 
+5
+12 9 61 5 14 
+'''
 nRange=input()
 numbers=list(map(int,input().split()))
 print(all([x>0 for x in numbers]) and any([str(x)==str(x)[::-1] for x in numbers]))
@@ -8,6 +13,7 @@ print(all([x>0 for x in numbers]) and any([str(x)==str(x)[::-1] for x in numbers
 #     if any(num == num[-1:] for num in integers):
 #         print("True")
 # else:print("False")
-
-
+'''
+output: True
+'''
 
