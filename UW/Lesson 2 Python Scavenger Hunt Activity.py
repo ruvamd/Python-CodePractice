14ccf2c19f2184956d9091e9038957282274700c
vadim rusu <ruvamd@gmail.com>
2021-10-18 19:16:11 -0700

    yoohoo

---
+++ UW/Lesson 2 Python Scavenger Hunt Activity.py
@@ -0,0 +1,13 @@
+num=int(input('enter the number from 0 to 10:'))
+if num<11:
+    print(0,1,2,3,4,5,6,7,8,9,10)
+else: print('the number is above 10')
+
+for i in range(0,11):
+    print(i)
+
+i=0
+while i<11:
+    print(i)
+    i+=1
+      
