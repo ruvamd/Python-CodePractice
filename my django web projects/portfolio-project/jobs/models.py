10c8f905f15a12f0d64f1d56ab595c0d3605093d
vadim rusu <ruvamd@gmail.com>
2021-10-20 00:24:16 -0700

    .

--- my django web projects/portfolio-project/jobs/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib import Job
-from .models import Job
-# Create your models here.
-admin.site.register(Job)
-
-class Job(models.Model):
-    image=models.ImageField(upload_to='images/')
-    summary=models.CharField(max_length=200)
