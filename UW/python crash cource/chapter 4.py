7a748de77c2b2b22c015ff8e15c2c1e094b4bfcc
vadim rusu <ruvamd@gmail.com>
2021-10-26 03:16:41 -0700

    .

--- UW/python crash cource/chapter 4.py
+++ UW/python crash cource/chapter 4.py
@@ -8,10 +8,19 @@
 # print('the last three items in the list are:',l3[-3:])
 
 #pizza
-pizzas=['pepperoni','mashrooms','fourmeats']
-friends_pizzas=pizzas[:]
-pizzas.append('cheese')
-friends_pizzas.append('veggie')
+# pizzas=['pepperoni','mashrooms','fourmeats']
+# friends_pizzas=pizzas[:]
+# pizzas.append('cheese')
+# friends_pizzas.append('veggie')
 
-print('my favorites pizzas are:',[n for n in pizzas])
-print('me friend ')
+# print('my favorites pizzas are:',[n for n in pizzas])
+# print('my friends favorite pizzas are:',[n for n in friends_pizzas])
+
+#buffet
+pizzas=('pepperoni','mashrooms','fourmeats','soup','bourger')
+for i in pizzas:
+    print(i)
+# pizzas[0]=('sushi')
+print('revised menu')
+pizzas=('a','b')
+print(pizzas)
