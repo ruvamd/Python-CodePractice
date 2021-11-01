25a22f38aa862a6cb681a9ed8ef737fba4c83fcb
vadim rusu <ruvamd@gmail.com>
2021-10-31 21:19:52 -0700

    .

---
+++ UW/python crash cource/chapter 6.py
@@ -0,0 +1,16 @@
+#person
+# per_inf={'first_name':'vadim','last_name':'rusu','age':'38','city':'seattle'}
+# print(per_inf)
+#personal numbers
+per_numb={'a':123,'b':456,'c':789,'d':987,'f':654}
+#print(per_numb['a'])
+
+# for key,value in per_numb.items():
+#     print(f'the name {key} has the following definition: {value} which is the favorite number')
+
+# for key in per_numb:
+#     print(key,per_numb[key])
+
+# [print(key,':',value) for key,value in per_numb.items()]
+#glossary
+
