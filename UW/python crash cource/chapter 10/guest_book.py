0103c16994afe023869415f5d7f3fb10ba676b64
vadim rusu <ruvamd@gmail.com>
2021-11-06 13:49:36 -0700

    .

--- UW/python crash cource/chapter 10/guest_book.py
+++ UW/python crash cource/chapter 10/guest_book.py
@@ -1,11 +1,11 @@
 prompt='enter your name:'
 prompt+='(enter the q for quit)'
-message="'\n'"
+message=""
 with open('UW/python crash cource/chapter 10/guest_book.txt','w') as f:
         while message!='q':
                 message=input(prompt)
                 print(f"greetings {message},the data was recording in the file quest_book")
-                f.write(message)
+                f.write(f'{message}\n')
                
 
 # prompt='enter your name:'
