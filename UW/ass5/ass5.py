769359bc98d9c96b23849832d00f01bb7fda2dbb
vadim rusu <ruvamd@gmail.com>
2021-11-10 23:27:44 -0800

    .

---
+++ UW/ass5/ass5.py
@@ -0,0 +1,6 @@
+x = 1
+y = 2
+def do_nothing(a, b, c):
+    print("do_nothing was called with:", a, b, c)
+    print("at the end of the trivial module")
+
---
+++ UW/ass5/trivial.py
@@ -0,0 +1,6 @@
+from UW.ass5.ass5 import do_nothing
+
+
+
+b=do_nothing(1,2,3)
+print(b)
