ad3c9f8efc46b7ee284b43c580cb007080040f53
vadim rusu <ruvamd@gmail.com>
2021-12-08 16:55:46 -0800

    .

---
+++ sys admin useful code/machine_info.py
@@ -0,0 +1,10 @@
+import platform
+
+print("Machine      :", platform.machine())
+print("Version      :", platform.python_version())
+print("Version tuple:", platform.python_version_tuple())
+print("Compiler     :", platform.python_compiler())
+print("Build        :", platform.python_build())
+print("Node         :", platform.node())
+print("Platform     :", platform.platform())
+print("System       :", platform.system())
