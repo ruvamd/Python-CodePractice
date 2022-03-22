3b73427706118718f8d6622f1c70088cc7d7f39c
vadim rusu <ruvamd@gmail.com>
2022-03-21 23:42:15 -0700

    yo

--- api/main.py
+++ api/main.py
@@ -19,7 +19,9 @@ pc={
             }
     }
 
-@app.get('')
+@app.get('/get-item/{item_id}/{name}')
+def get_item(item_id:int,name:str):
+    return pc[item_id]
 
 # @app.get("/")
 # def read_root():
@@ -33,6 +35,6 @@ pc={
 # def about():
 #     return{'data':'about'}
 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
