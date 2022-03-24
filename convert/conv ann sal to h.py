fcc38499ea426e433b2381d9fc1034a803afda6b
vadim rusu <ruvamd@gmail.com>
2022-03-24 00:57:51 -0700

    .

--- convert/conv ann sal to h.py
+++ convert/conv ann sal to h.py
@@ -1,4 +1,9 @@
-#condition of work: 12 month,four weeks,five days a week and eight hours a day 
+'''condition of work: 12 month,
+                      four weeks,
+                      five days a week, 
+                      eight hours a day. 
+'''
+
 yearSal=int(input('Enter the year salary: '))
 def convYearToHourSal():
     return round(yearSal/12/4/5/8,2)
