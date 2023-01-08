ce571e6e3437fe0bfa00610081c5d9652dc110ef
vadim rusu <ruvamd@gmail.com>
2023-01-08 15:53:17 -0800

    Binary Tree Inorder Traversal

--- practice.py
+++ practice.py
@@ -1,8 +1,19 @@
-from math import sqrt,floor
+class tree_node:
+    def __init__(self,val=0,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
 
-def mySqrt(x):
-    x=sqrt(x)
-    return floor(x)
+class Solution:
+    def inorder_traversal(self,root):
+        if root is None:
+            return []
+        return self.inorder_traversal(root.left)+[root.val]+self.inorder_traversal(root.right)
 
-x=8
-print(mySqrt(x))
+if __name__=="__main__":
+    root=tree_node(1)
+    root.right=tree_node(2)
+    root.left=tree_node(3)
+    print(Solution().inorder_traversal(root))
+
+
