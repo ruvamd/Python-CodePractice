c9f36d363756e64c25aa881250f78ae45b4515ae
vadim rusu <ruvamd@gmail.com>
2023-01-07 20:48:48 -0800

    .

---
+++ Climbing Stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        a = 1
+        b = 2
+        for i in range(3, n + 1):
+            c = a + b
+            a = b
+            b = c
+        return c
+
+n=3
+print(Solution().climbStairs(n))
