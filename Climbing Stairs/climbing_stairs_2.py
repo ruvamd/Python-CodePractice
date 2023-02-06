ba8871762af0cc32e8efc5f1cbfd42452dc45423
vadim rusu <ruvamd@gmail.com>
2023-02-05 19:22:21 -0800

    .

--- Climbing Stairs/climbing_stairs_2.py
+++ Climbing Stairs/climbing_stairs_2.py
@@ -3,7 +3,8 @@ def step_ways(steps):
         return 0
     elif steps == 0:
         return 1
+
     else:
         return step_ways(steps - 1) + step_ways(steps - 2) + step_ways(steps - 3)
 
-print(step_ways(4))
+print(step_ways(1))
