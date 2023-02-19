8bba17cb3aff7dca73f62a7c64a3133d630c467b
vadim rusu <ruvamd@gmail.com>
2023-02-18 21:00:23 -0800

    rpn calculator and remove max

--- practice.py
+++ practice.py
@@ -1,44 +1,75 @@
-def SayHiNTimes(n):
-    x = 0
-    while x < n:
-        x += 1
-        print("hi")
-        
-SayHiNTimes(5)
+# Python3 code to print happy valentine's
+# day
+import math
 
-def Sort(numbers):
-    for x in range(len(numbers)):
-        for y in range(len(numbers)):
-            if numbers[x] < numbers[y]:
-                temp = numbers[x]
-                numbers[x] = numbers[y]
-                numbers[y] = temp
+n = 10
 
-def Display(myList):
-  for num in myList:
-   print(num)
+# Initializing String to print in heart
+message = " HappY Valentines DaY <3 "
 
-num_list = [5, 1, 8, 2]
+# Position from where from top
+# message box would be placed.
+print_message = 4
 
-Display(num_list)
+# Add space if message length is odd
+if (len(message) % 2 != 0):
+	message += " "
 
-Sort(num_list)
+# Outer loop to adjust length of upper
+# part message is not handled in this part
+for a in range(n):
+	
+	# To print space and variable accordingly
+	for b in range(4 * n + 1):
+		
+		# Computing distance to print variable
+		distance1 = math.sqrt(pow(a - n, 2) +
+							pow(b - n, 2))
+		distance2 = math.sqrt(pow(a - n, 2) +
+							pow(b - 3 * n, 2))
+							
+		if (distance1 < n + 0.5 or
+			distance2 < n + 0.5):
+			print("*", end = "")
+		else:
+			print(" ", end = "")
 
-Display(num_list)
+	# Ending line after each iteration
+	print()
 
-def PrintFirstItemThenFirstHalfThenSayHi100Times(int_list):
-  print(int_list[0])
+# Printing the message part and lower
+# part of heart. Outer loop handles
+# depth of the heart.
+for a in range(1, 2 * n):
 
-  middleIndex = len(int_list)/2
-  index = 0
+	# For getting the lower curve of heart
+	for b in range(a):
+		print(" ", end = "")
+		
+	# Inner loop
+	# handles the message and spaces accordingly
+	for b in range(4 * n + 1 - 2 * a):
+		
+		# Checks if the height is in range of
+		# message space
+		if (a >= print_message - 1 and
+			a <= print_message + 1):
+			point = b - (4 * n - 2 *
+					a - len(message)) // 2
 
-  while index < middleIndex:
-      print(int_list[index])
-      index += 1
+			# Prints message after leaving
+			# appropriate space
+			if (point < len(message) and point >= 0):
+				if (a == print_message):
+					print(message[point], end = "")
+				else:
+					print(" ", end = "")
+			else:
+				print("*", end = "")
 
-  x = 0
-  while x < 100:
-      x += 1
-      print("hi")
-      
-PrintFirstItemThenFirstHalfThenSayHi100Times(num_list)
+		else:
+			print("*", end = "")
+
+	print()
+
+# This code is contributed by subhammahato348
