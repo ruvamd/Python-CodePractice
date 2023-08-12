fbfea9137566b73fe2268e4fc9958f8b6b8e9eb2
vadim rusu <ruvamd@gmail.com>
2023-08-12 11:03:34 -0700

    .

--- practice.py
+++ practice.py
@@ -1,26 +1,30 @@
 from typing import List
 
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
         
-        while left <= right:
-            mid = left + (right - left) // 2
+        triangle = [[1]]  # Initialize with the first row
+        
+        for i in range(1, numRows):
+            prev_row = triangle[i - 1]
+            new_row = [1]
             
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+            for j in range(1, i):
+                new_row.append(prev_row[j - 1] + prev_row[j])
+            
+            new_row.append(1)
+            triangle.append(new_row)
         
-        return left
+        return triangle
 
 solution = Solution()
 
-nums = [1, 3, 5, 6]
-target = 5
-print(solution.searchInsert(nums, target))  # Output: 2
+numRows = 5
+print(solution.generate(numRows))
+# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
-target = 2
-print(solution.searchInsert(nums, target))  # Output: 1
+numRows = 1
+print(solution.generate(numRows))
+# Output: [[1]]
