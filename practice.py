1b10f7fca6d6d658c89b61b7da36608c963849ae
vadim rusu <ruvamd@gmail.com>
2023-08-15 01:00:56 -0700

    .

--- practice.py
+++ practice.py
@@ -1,30 +1,11 @@
-from typing import List
-
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        
-        triangle = [[1]]  # Initialize with the first row
-        
-        for i in range(1, numRows):
-            prev_row = triangle[i - 1]
-            new_row = [1]
-            
-            for j in range(1, i):
-                new_row.append(prev_row[j - 1] + prev_row[j])
-            
-            new_row.append(1)
-            triangle.append(new_row)
-        
-        return triangle
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)).lower()
+        return s == s[::-1]
 
 solution = Solution()
 
-numRows = 5
-print(solution.generate(numRows))
-# Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-
-numRows = 1
-print(solution.generate(numRows))
-# Output: [[1]]
+print(solution.isPalindrome("racecar"))  # Output: True
+print(solution.isPalindrome("hello"))    # Output: False
+print(solution.isPalindrome("madam"))    # Output: True
+print(solution.isPalindrome("Able was I ere I saw Elba"))  # Output: True
