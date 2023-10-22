dc62ccc0a88cbb75cf0e28dedc1d7824a0189446
vadim rusu <ruvamd@gmail.com>
2023-10-21 19:25:42 -0700

    .

--- practice.py
+++ practice.py
@@ -1,21 +1,31 @@
 from typing import List
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        # Initialize variables to keep track of the current and maximum subarray sums
-        current_sum = max_sum = nums[0]
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_gas = 0  # Total gas available from all stations.
+        total_cost = 0  # Total cost to travel between all stations.
+        current_gas = 0  # Gas available at the current station.
+        start_index = 0  # Starting station index.
 
-        # Iterate through the array starting from the second element
-        for i in range(1, len(nums)):
-            # Calculate the current sum by choosing the larger of the current element
-            # or the current element combined with the current subarray sum
-            current_sum = max(nums[i], current_sum + nums[i])
+        for i in range(len(gas)):
+            # Calculate the difference between gas available and the cost at the current station.
+            diff = gas[i] - cost[i]
 
-            # Update the maximum subarray sum if the current sum is larger
-            max_sum = max(max_sum, current_sum)
+            # Update the current gas and total values.
+            current_gas += diff
+            total_gas += diff
+            total_cost += diff
 
-        return max_sum
-    
-nums = [-2,1,-3,4,-1,2,1,-5,4]
-sol = Solution()
-print(sol.maxSubArray(nums))
+            # If running out of gas at this station, update the start index to the next station.
+            if current_gas < 0:
+                current_gas = 0
+                start_index = i + 1
+
+        # If the total gas available is greater than or equal to the total cost, a valid route exists.
+        if total_gas >= total_cost:
+            return start_index
+        else:
+            return -1  # No valid route exists.
+
+gas = [1,2,3,4,5]
+cost = [3,4,5,1,2]
